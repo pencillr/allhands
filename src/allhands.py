266600e0713223b2f6080050654cbc8b37da7280
Richard Lenkovits <richardlenkovits@192.168.1.31>
2019-06-21 16:12:20 +0200

    Add login logic and modules

--- src/allhands.py
+++ src/allhands.py
@@ -1,1 +1,10 @@
-from app import app
+from app import app, db
+from app.models import User, Post
+
+
+# When the flask shell command runs,
+# it will invoke this function and register
+# the items returned by it in the shell session.
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
