5bd738f3da6b16a5f86d8c1e8e5a3feb10e1b07c
Richard Lenkovits <richardlenkovits@192.168.1.31>
2019-06-21 14:28:02 +0200

    Add SQLAlchemy and database base

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,8 +1,12 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 # This bottom import is a workaround to circular imports,
 # a common problem with Flask applications.
@@ -10,4 +14,4 @@ app.config.from_object(Config)
 # so putting one of the reciprocal imports at the bottom avoids the error
 # that results from the mutual references between these two files.
 
-from app import routes
+from app import routes, models
