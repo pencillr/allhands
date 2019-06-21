84bb66497a8ad3b24760f0cd9831137cf11efee6
Richard Lenkovits <richardlenkovits@192.168.1.31>
2019-06-21 14:51:12 +0200

    Add Post model base

--- src/app/models.py
+++ src/app/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from app import db
 
 class User(db.Model):
@@ -5,6 +6,21 @@ class User(db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
+    # This is not an actual database field, but a high-level view
+    # of the relationship between users and posts,
+    # and for that reason it isn't in the database diagram.
+    # This  is for user -> post queries
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id')) # user.id:
+    # model is given by its database table name, for which SQLAlchemy automatically
+    # uses lowercase characters and, for multi-word model names, snake case.
 
     def __repr__(self):
-        return '<User {}>'.format(self.username)    
+        return '<Post {}>'.format(self.body)
