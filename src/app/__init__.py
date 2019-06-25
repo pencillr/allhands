347f607493d6ceaeb122976f6d89f4e7dee5d3c6
Pencillr <pencillr@gmail.com>
2019-06-25 14:07:53 +0200

    Add profile management

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -3,6 +3,8 @@ from flask_login import LoginManager
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+import logging
+from logging.handlers import SMTPHandler
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -11,10 +13,26 @@ login.login_view = 'login' # view function that handles logins
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+if not app.debug:
+    if app.config['MAIL_SERVER']:
+        auth = None
+        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+        secure = None
+        if app.config['MAIL_USE_TLS']:
+            secure = ()
+        mail_handler = SMTPHandler(
+            mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+            fromaddr='no-reply@' + app.config['MAIL_SERVER'],
+            toaddrs=app.config['ADMINS'], subject='Microblog Failure',
+            credentials=auth, secure=secure)
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
+
 # This bottom import is a workaround to circular imports,
 # a common problem with Flask applications.
 # Routes module needs to import the app variable defined in this script,
 # so putting one of the reciprocal imports at the bottom avoids the error
 # that results from the mutual references between these two files.
 
-from app import routes, models
+from app import routes, models, errors
