8e32725e12d0a8273dd25497c7e6fbeb4ece4529
Pencillr <pencillr@gmail.com>
2019-06-26 13:23:20 +0200

    Add password reset

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_login import LoginManager
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
+from flask_mail import Mail
 from flask_migrate import Migrate
 import logging
 from logging.handlers import SMTPHandler
@@ -12,6 +13,7 @@ login = LoginManager(app)
 login.login_view = 'login' # view function that handles logins
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+mail = Mail(app)
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
